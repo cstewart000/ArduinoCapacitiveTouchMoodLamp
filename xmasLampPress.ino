08cb7b0a656f9638db8a7587e4ff4251e7683bd9
cstewart000 <cstewart000@gmail.com>
2016-12-27 21:12:58 +1300

    Titled, commented and refactored code

--- xmasLampPress.ino
+++ xmasLampPress.ino
@@ -1,42 +1,70 @@
+/*
+    Arduino Capacitive Touch Mood Lamp
+
+    A simple program for a Mood Lamp with a single imput, a capacitive touch surface, 
+    which detects different press types (ie short, long) to change the behaviour. 
+    The program uses the AdaFroit NeoPixel library to drive RGB LEDs.
+
+    The circuit:
+
+      - D3 : Connected to the signal line of the LED strip.
+      - D11 : Connected to a 10M resistor and to the touch surface.
+      - D12 : Connected to touch surface. 
+
+      - Power where it should be :P
+
+      Created: 24/12/2016
+      By: Cameron Stewart, cstewart000@gmail.com
+
+
+      https://github.com/cstewart000/ArduinoCapacitiveTouchMoodLamp
+*/
+
+
+// Libraries
 #include <Adafruit_NeoPixel.h>
 #include <CapacitiveSensor.h>
 
-
-#define capacitiveThreshold 25
-#define CAPACITIVE_SAMPLES 100
-
+// Hardware definitions
 #define CAPPIN1 11
 #define CAPPIN2 12
 #define STRIP_PIN 3
+#define NUM_PIXELS 36
 
+// Capacitive touch sensor configuration
+#define capacitiveThreshold 25
+#define CAPACITIVE_SAMPLES 100
+
+// Press min duration definition 
 #define SHORT_PRESS 100
 #define LONG_PRESS 500
 
+// Number of different mood routines to be defined
 #define NUM_MODES 2
 
+// Initialisaiton of library objects
 CapacitiveSensor   cs = CapacitiveSensor(CAPPIN1,CAPPIN2); 
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_PIXELS, STRIP_PIN, NEO_GRB + NEO_KHZ800);
 
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(36, STRIP_PIN, NEO_GRB + NEO_KHZ800);
 
+// Global state variables
 long buttonTimer = 0;
 long time;
 long buttonTime = 0;
-
 long longPressTime = 250;
-
 
 boolean buttonState = false;
 boolean buttonLastState = false;
 boolean longPressActive = false;
+boolean lampOn =false;
 
+int mode = 1;
+
+// Method declarations
 boolean checkButtonState();
 int pressType(int);
 void lampStateManager(int);
 void updateLamp();
-
-boolean lampOn =false;
-int mode = 1;
-
 
 
 void setup() {
@@ -45,97 +73,59 @@ void setup() {
     Serial.begin(9600);
     strip.begin();
     strip.show(); // Initialize all pixels to 'off'
-
-
 }
 
 void loop() {
   
+  // TODO: Check button state in interrupt to stop code blocking
   buttonState = checkButtonState();
-  
   
   // New press
   if(buttonState && !buttonLastState){
+    
     Serial.println("New press");
     buttonLastState =true; 
     buttonTimer = 0;
     buttonTime = millis();
+  
   }
   
-  // continue press
+  // Continue press
   if(buttonState){
-    buttonLastState =true;
-    //Serial.println("Continued press");
-    buttonTimer = millis() - buttonTime;
+
+    Serial.println("Continued press");
+    buttonLastState = true;
+    buttonTimer = millis() - buttonTime; // record time that the button has been depressed.
     
   }
   
   // release
   if(!buttonState && buttonLastState){
-    buttonLastState =false;
-
     
-    int pressOption = pressType(buttonTimer);
-    
-    lampStateManager(pressOption);
+    Serial.println("Released press");
+    buttonLastState = false;
+    int pressOption = pressType(buttonTimer); // determine the type of press from depression time
+    lampStateManager(pressOption); // Act based on press type.
     
     buttonTimer = 0;
   }
   
+  // Update the lamp based on changed state variables.
   updateLamp();
   
+  // Debug logging
   Serial.print("lamp state:");
   Serial.print(lampOn);
   Serial.print("\tlamp mode:");
   Serial.println(mode);
   
-
 }
-/*
-
-	if (digitalRead(button) == HIGH) {
-
-		if (buttonActive == false) {
-
-			buttonActive = true;
-			buttonTimer = millis();
-
-		}
-
-		if ((millis() - buttonTimer > longPressTime) && (longPressActive == false)) {
-
-			longPressActive = true;
-			LED1State = !LED1State;
-			digitalWrite(LED1, LED1State);
-
-		}
-
-	} else {
-
-		if (buttonActive == true) {
-
-			if (longPressActive == true) {
-
-				longPressActive = false;
-
-			} else {
-
-				LED2State = !LED2State;
-				digitalWrite(LED2, LED2State);
-
-			}
-
-			buttonActive = false;
-
-		}
-
-	}
-*/
 
 
 boolean checkButtonState(){
+
   long capacitiveSwitchValue = cs.capacitiveSensor(CAPACITIVE_SAMPLES);
-  
+
   if(capacitiveSwitchValue>capacitiveThreshold){
     return true;
   }else{
@@ -149,6 +139,15 @@ int pressType(int buttonTimer){
     return 1;
   }else if(buttonTimer>SHORT_PRESS){
     return 2;
+
+    // Add more branching options here for more other behaviours
+
+    // TODO: Held press - scroll over the colour wheel.
+
+    // TODO: Double tap - step forward
+    
+    // TODO: Triple tap - step back
+
   }else
   return 0;    
   
@@ -179,36 +178,33 @@ void lampStateManager(int pressOption){
 void updateLamp(){
   
   if(!lampOn){
-    colorWipe(strip.Color(0, 0, 0), 50); // off
+    colourStripSet(strip.Color(0, 0, 0)); // off
   }else{
    
    switch(mode%NUM_MODES){
     case 0:
-    colorWipe(strip.Color(222, 40, 179), 50); // 
+    colourStripSet(strip.Color(222, 40, 179)); // 
     break;
     
     case 1:
-    colorWipe(strip.Color(224, 169, 114), 50); //
+    colourStripSet(strip.Color(224, 169, 114)); //
     break;
     
     case 2:
-    colorWipe(strip.Color(random(255), random(255), random(255)), 50); //
+    colourStripSet(strip.Color(random(255), random(255), random(255))); //
     break;
     
+    // TODO: Add case statements for new behaviours
     
    }
    strip.show();
   }
-  
-  
-  
-  
 }
 
-
-void colorWipe(uint32_t c, uint8_t wait) {
+// Sets strip to a new colour
+void colourStripSet(uint32_t colour {
   for(uint16_t i=0; i<strip.numPixels(); i++) {
-    strip.setPixelColor(i, c);
+    strip.setPixelColor(i, colour);
   }
   strip.show();
 }
